e63fd685271cb53d01e185256bd3163f7aa239f4
Richard Caceres <richard@archive.org>
2017-11-05 15:43:11 -0800

    Add route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import {MyBooks} from './components/pages/MyBooks';
 import {Form} from './components/pages/Form';
 import {Browse} from './components/pages/Browse';
 import {Details} from './components/pages/Details';
+import {Search} from './components/pages/Search';
 
 export const routes = [{
     path: '/mybooks/',
@@ -12,6 +13,9 @@ export const routes = [{
 }, {
     path: '/details/:identifier',
     component: Details
+}, {
+    path: '/search/:query',
+    component: Search
 }, {
     path: '/',
     component: Browse
